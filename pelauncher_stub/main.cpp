69939b840b1dcfcc29ee2666047c322bfd4b43ce
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-08-20 21:21:38 +0300

    [STUB] Enable theming

--- pelauncher_stub/main.cpp
+++ pelauncher_stub/main.cpp
@@ -1,6 +1,9 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+// Theming:
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
